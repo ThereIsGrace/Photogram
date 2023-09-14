f4b87de4ab326b2c39d586245a3c04ea45cdd1b1
ThereIsGrace <universe2879@gmail.com>
2023-09-14 17:03:55 +0900

    프로필 페이지 완성

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -9,21 +9,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.user.User;
+import com.cos.photogramstart.dto.user.UserProfileDto;
+import com.cos.photogramstart.service.UserService;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class UserController {
 	
-	@GetMapping("/user/{id}")
-	public String profile(@PathVariable int id) {
+	private final UserService userService;
+	
+	@GetMapping("/user/{pageUserId}")
+	public String profile(@PathVariable int pageUserId, Model model, @AuthenticationPrincipal PrincipalDetails principalDetails) {
+		UserProfileDto dto = userService.회원프로필(pageUserId, principalDetails.getUser().getId());
+		model.addAttribute("dto", dto);
 		return "user/profile";
 	}
 	
 	@GetMapping("/user/{id}/update")
 	public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails) {
-		System.out.println("세션 정보: " + principalDetails.getUser());
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		PrincipalDetails mPrincipalDetails = (PrincipalDetails)auth.getPrincipal();
-		System.out.println("직접 찾은 세션 정보 : "+mPrincipalDetails.getUser());
+		// 1.추천
+		//System.out.println("세션 정보: " + principalDetails.getUser());
+		
+		// 2.극혐
+		//Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		//PrincipalDetails mPrincipalDetails = (PrincipalDetails)auth.getPrincipal();
+		//System.out.println("직접 찾은 세션 정보 : "+mPrincipalDetails.getUser());
 	
 		return "user/update";
 	}
